8a4f143212d3deeb9d44aa70212c7f2f4246c152
aztpmc <aztpmc@gmail.com>
2026-08-10 14:32:14 +0300

    Delete tests directory

--- tests/support/shipped-core-entry.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-// Adapter so compare-golden-fixture.js can target the currently shipped
-// numbers-to-words.html the same way it will later target src/core/index.js.
-const { loadShippedCore, SHIPPED_HTML_PATH } = require('./load-shipped-core');
-module.exports = loadShippedCore(SHIPPED_HTML_PATH);
